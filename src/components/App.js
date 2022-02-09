b4ad2651bcc2a1b491c863d07ce558a88d226524
Jonathan Bryant <spectrum19tech@gmail.com>
2022-02-09 14:53:06 -0800

    commit tree cae172ce92ee031b8ae4b069cfce0f5c82e8d178

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
@@ -6,6 +6,39 @@ import PetBrowser from "./PetBrowser";
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  function onChangeType(e) {
+    setFilters({ type: e.target.value })
+  }
+
+  // Adding in initial fetch of everything to make sure onAdoptPet works
+  useEffect(() => {
+    fetch('http://localhost:3001/pets')
+      .then(r => r.json())
+      .then(setPets)
+  }, [])
+
+
+
+  function onFindPetsClick() {
+    let fetchURL = ``
+    if (filters.type === 'all') {
+      fetchURL = 'http://localhost:3001/pets'
+    } else {
+      fetchURL = `http://localhost:3001/pets?type=${filters.type}`
+    }
+    fetch(fetchURL)
+      .then(r => r.json())
+      .then(setPets)
+  }
+
+  function onAdoptPet(id) {
+    const foundPet = pets.find(pet => pet.id === id)
+    foundPet.isAdopted = true
+    setPets([...pets])
+  }
+
+
 
   return (
     <div className="ui container">
@@ -15,10 +48,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick}/>
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet}/>
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -2,8 +2,18 @@ import React from "react";
 
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser( {pets, onAdoptPet} ) {
+  
+  const petCard = pets.map(pet => {
+    // console.log(pet.id)
+    return <Pet key={pet.id} id={pet.id} name={pet.name} type={pet.type} age={pet.age} weight={pet.weight} isAdopted={pet.isAdopted} gender={pet.gender} onAdoptPet={onAdoptPet}/>
+  })
+  
+  return (
+    <div className="ui cards">
+      {petCard}
+    </div>
+  )
 }
 
 export default PetBrowser;
